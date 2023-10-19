9610e2ab382580b6a89b53cb5ad55379010224bd
David Bridges <dbridges@cpocommerce.com>
2023-10-18 20:27:46 -0700

    added main with suspect code for scanning

---
+++ src/main/java/com/cyclemost/objectfilter/Program.java
@@ -0,0 +1,13 @@
+package com.cyclemost.objectfilter;
+
+public static void main(String args[]) {
+    String username = "username";
+    String password = "password";
+    String secret_key = "asdlkj13879asdfkjgk";
+
+    authenticate(username, password, secret_key);
+}
+
+public static boolean authenticate(String username, String password, String secret_key) {
+    return true;
+}
